9daa7bed501c40a419cc91cd3b2a8f16b6f30823
abdallahEid <abdallaheidm@gmail.com>
2019-06-17 00:26:24 +0200

    Making the basic design of the first screen

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stopRecordingButton: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingStatus: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startRecording(_ sender: Any) {
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+    }
 }
 
